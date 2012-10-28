3281516cdf4d995207ca23d30547abf38d38c170
alfred <alfred@ubuntu.(none)>
2012-10-28 04:01:29 -0700

    add extractcenter method

--- Src/block_manager.f90
+++ Src/block_manager.f90
@@ -29,30 +29,49 @@ CONTAINS
     ALLOCATE(Blk%X(-1:im+2,-1:jm+2));ALLOCATE(Blk%Y(-1:im+2,-1:jm+2))
     ALLOCATE(Blk%X_Center(-1:im+2,-1:jm+2));ALLOCATE(Blk%Y_Center(-1:im+2,-1:jm+2))
 
+    ALLOCATE(Blk%ro(-1:im+2,-1:jm+2));
+    ALLOCATE(Blk%rou(-1:im+2,-1:jm+2));
+    ALLOCATE(Blk%rov(-1:im+2,-1:jm+2));
+    ALLOCATE(Blk%row(-1:im+2,-1:jm+2));
+    ALLOCATE(Blk%roE(-1:im+2,-1:jm+2));
+
   END SUBROUTINE ALLOCATE_BLOCK
 
   !=============================================================
   !=============================================================
 
-  SUBROUTINE EXTRACT_MESH(Blk)
+  SUBROUTINE EXTRACT_MESH(Blk,ExtractType)
     IMPLICIT NONE
     !*************************************************************
     TYPE(BLOCK) :: Blk
     INTEGER :: im,jm
     INTEGER :: i,j
+    CHARACTER(len=*) :: ExtractType
     !*************************************************************
 
     im = Blk%im
     jm = Blk%jm
 
-    write(*,'(A19,A)') "Ecriture du fichier : ", './mesh.tp'
-    open(unit=1,file='./mesh.tp',status='replace',action='write')
-    write(1,*) 'TITLE="Maillage "'
-    write(1,*) 'VARIABLES = "X" "Y"'
-    write(1,*) 'ZONE T= " ", I=',im,'J=',jm,'F=BLOCK'
-    write(1,*) ((Blk%X(i,j),i=1,im),j=1,jm)
-    write(1,*) ((Blk%Y(i,j),i=1,im),j=1,jm)
-    close(1)
+
+    IF(ExtractType.EQ.'n')THEN
+       write(*,'(A19,A)') "Ecriture du fichier : ", './mesh.tp'
+       open(unit=1,file='./mesh.tp',status='replace',action='write')
+       write(1,*) 'TITLE="Maillage "'
+       write(1,*) 'VARIABLES = "X" "Y"'
+       write(1,*) 'ZONE T= " ", I=',im,'J=',jm,'F=BLOCK'
+       write(1,*) ((Blk%X(i,j),i=1,im),j=1,jm)
+       write(1,*) ((Blk%Y(i,j),i=1,im),j=1,jm)
+       close(1)
+    ELSEIF(ExtractType.EQ.'center')THEN
+       write(*,'(A19,A)') "Ecriture du fichier : ", './meshcenter.tp'
+       open(unit=1,file='./meshcenter.tp',status='replace',action='write')
+       write(1,*) 'TITLE="Maillage "'
+       write(1,*) 'VARIABLES = "X" "Y"'
+       write(1,*) 'ZONE T= " ", I=',im,'J=',jm,'F=BLOCK'
+       write(1,*) ((Blk%X_Center(i,j),i=1,im),j=1,jm)
+       write(1,*) ((Blk%Y_Center(i,j),i=1,im),j=1,jm)
+       close(1)
+    END IF
 
   END SUBROUTINE EXTRACT_MESH
 
